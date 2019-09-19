f9102d146a1324abf9bd2943d8ababf98df199df
usermeng1234 <164033893@qq.com>
2019-09-19 22:46:16 +0800

    '完善项目'

---
+++ src/service.config.js
@@ -0,0 +1,9 @@
+const MOCKURL='http://www.weichuang.com/';
+const SERVERRUL='http://localhost:3000/';
+
+const URL={
+    getVarietyItme:MOCKURL+'getVarietyItme',
+    getHotProduct:MOCKURL+'getHotProduct',
+    registUser:SERVERRUL+'user/registUser'
+};
+export default URL;
--- src/views/My.vue
+++ src/views/My.vue
@@ -1,17 +1,60 @@
 <template>
     <div>
-        <van-nav-bar title="我的">
-            <van-icon name="search" slot="left"></van-icon>
-            <van-icon name="cart" slot="right"></van-icon>
-        </van-nav-bar>
+        <van-tabs>
+            <van-tab title="登录">
+                <van-cell-group>
+                    <van-field label="用户名" required placeholder="请输入用户名" v-model="loginUsername"></van-field>
+                    <van-field label="密码" type="password" required placeholder="请输入密码" v-model="loginPassword" ></van-field>
+                </van-cell-group>
+
+                <div>
+                    <van-button @click="registerHander" type="primary" size="large">登录</van-button>
+                </div>
+            </van-tab>
+
+            <van-tab title="注册">
+                <van-cell-group>
+                    <van-field label="用户名" required placeholder="请输入用户名" v-model="registerUsername" ></van-field>
+                    <van-field label="密码" type="password" required placeholder="请输入密码" v-model="registerPassword" ></van-field>
+                </van-cell-group>
+
+                <div>
+                    <van-button type="primary" size="large">注册</van-button>
+                </div>
+            </van-tab>
+
+        </van-tabs>
     </div>
 </template>
 
 <script>
-
-
+import axios from 'axios';
+import url from '@/service.config.js'
     export default {
-
+    data(){
+        return{
+            loginUsername:'',
+            loginPassword:'',
+            registerUsername:'',
+            registerPassword:''
+        }
+    },
+    methods:{
+        registerHander(){
+            axios({
+                url:url.registUser,
+                method:'post',
+                data:{
+                    userName:this.registerUsername,
+                    password:this.registerPassword
+                }
+            }).then(res=>{
+                console.log(res)
+            }).catch(err=>{
+                console.log(err)
+            });
+        }
+    }
     }
 </script>
 <style lang="scss">
